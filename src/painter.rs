7006a352dc5d88322a47e8df6911ee8b1ccbbe54
Emil Ernerfeldt <emilernerfeldt@gmail.com>
2021-01-23 00:26:46 +0100

    Do color multiplication in linear space in the fragment shader

--- src/painter.rs
+++ src/painter.rs
@@ -27,7 +27,7 @@ impl Painter {
             &[
                 VertexAttribute::new("a_pos", VertexFormat::Float2),
                 VertexAttribute::new("a_tc", VertexFormat::Float2),
-                VertexAttribute::new("a_color", VertexFormat::Byte4),
+                VertexAttribute::new("a_srgba", VertexFormat::Byte4),
             ],
             shader.expect("couldn't make shader"),
             PipelineParams {
@@ -171,15 +171,29 @@ impl Painter {
 mod shader {
     use miniquad::{ShaderMeta, UniformBlockLayout, UniformDesc, UniformType};
 
-    pub const VERTEX: &str = r#"#version 100
+    pub const VERTEX: &str = r#"
+    #version 100
     uniform vec2 u_screen_size;
 
     attribute vec2 a_pos;
     attribute vec2 a_tc;
-    attribute vec4 a_color;
+    attribute vec4 a_srgba;
 
     varying vec2 v_tc;
-    varying vec4 v_color;
+    varying vec4 v_rgba;
+
+    // 0-1 linear  from  0-255 sRGB
+    vec3 linear_from_srgb(vec3 srgb) {
+        bvec3 cutoff = lessThan(srgb, vec3(10.31475));
+        vec3 lower = srgb / vec3(3294.6);
+        vec3 higher = pow((srgb + vec3(14.025)) / vec3(269.025), vec3(2.4));
+        return mix(higher, lower, vec3(cutoff));
+    }
+
+    // 0-1 linear  from  0-255 sRGBA
+    vec4 linear_from_srgba(vec4 srgba) {
+        return vec4(linear_from_srgb(srgba.rgb), srgba.a / 255.0);
+    }
 
     void main() {
         gl_Position = vec4(
@@ -189,17 +203,55 @@ mod shader {
             1.0);
 
         v_tc = a_tc;
-        v_color = a_color / 255.0;
-    }"#;
+        v_rgba = linear_from_srgba(a_srgba);
+    }
+    "#;
 
-    pub const FRAGMENT: &str = r#"#version 100
+    pub const FRAGMENT: &str = r#"
+    #version 100
     uniform sampler2D u_sampler;
     precision highp float;
+
     varying vec2 v_tc;
-    varying vec4 v_color;
+    varying vec4 v_rgba;
+
+    // 0-1 linear  from  0-255 sRGB
+    vec3 linear_from_srgb(vec3 srgb) {
+        bvec3 cutoff = lessThan(srgb, vec3(10.31475));
+        vec3 lower = srgb / vec3(3294.6);
+        vec3 higher = pow((srgb + vec3(14.025)) / vec3(269.025), vec3(2.4));
+        return mix(higher, lower, vec3(cutoff));
+    }
+
+    // 0-1 linear  from  0-255 sRGBA
+    vec4 linear_from_srgba(vec4 srgba) {
+        return vec4(linear_from_srgb(srgba.rgb), srgba.a / 255.0);
+    }
+
+    // 0-255 sRGB  from  0-1 linear
+    vec3 srgb_from_linear(vec3 rgb) {
+        bvec3 cutoff = lessThan(rgb, vec3(0.0031308));
+        vec3 lower = rgb * vec3(3294.6);
+        vec3 higher = vec3(269.025) * pow(rgb, vec3(1.0 / 2.4)) - vec3(14.025);
+        return mix(higher, lower, vec3(cutoff));
+    }
+
+    // 0-255 sRGBA  from  0-1 linear
+    vec4 srgba_from_linear(vec4 rgba) {
+        return vec4(srgb_from_linear(rgba.rgb), 255.0 * rgba.a);
+    }
+
     void main() {
-        gl_FragColor = v_color;
-        gl_FragColor *= texture2D(u_sampler, v_tc);
+        vec4 texture_srgba = texture2D(u_sampler, v_tc);
+        vec4 texture_rgba = linear_from_srgba(texture2D(u_sampler, v_tc) * 255.0); // TODO: sRGBA aware sampeler, see linear_from_srgb;
+        gl_FragColor = v_rgba * texture_rgba;
+
+        // miniquad doesn't support linear blending in the framebuffer.
+        // so we need to convert linear to sRGBA:
+        gl_FragColor = srgba_from_linear(gl_FragColor) / 255.0; // TODO: sRGBA aware framebuffer
+
+        // We also apply this hack to at least get a bit closer to the desired blending:
+        gl_FragColor.a = pow(gl_FragColor.a, 1.6); // Empiric nonsense
     }
     "#;
 
