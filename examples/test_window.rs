478d2ffbc540ff2406947f99cef7bdc16ba2f253
Emil Ernerfeldt <emilernerfeldt@gmail.com>
2021-01-23 11:29:17 +0100

    Hook up keyboard events and scroll wheel

--- examples/test_window.rs
+++ examples/test_window.rs
@@ -1,90 +1,63 @@
-use {
-    egui::{pos2, vec2},
-    emigui_miniquad::Painter,
-    miniquad::{self as mq, conf, Context, EventHandler},
-};
+use {emigui_miniquad as egui_mq, miniquad as mq};
 
 struct Stage {
     egui_ctx: egui::CtxRef,
-    raw_input: egui::RawInput,
-    painter: Painter,
+    egui_input: egui::RawInput,
+    painter: egui_mq::Painter,
+
+    show_egui_demo_windows: bool,
+
+    egui_demo_windows: egui_demo_lib::DemoWindows,
 }
 
 impl Stage {
     fn new(ctx: &mut mq::Context) -> Self {
-        let egui_ctx = egui::CtxRef::default();
-
-        let pixels_per_point = ctx.dpi_scale();
-        let (width, height) = ctx.screen_size();
-        let screen_size = vec2(width as f32, height as f32) / pixels_per_point;
-
-        let raw_input = egui::RawInput {
-            screen_rect: Some(egui::Rect::from_min_size(Default::default(), screen_size)),
-            pixels_per_point: Some(pixels_per_point),
-            ..Default::default()
-        };
-
         Self {
-            egui_ctx,
-            painter: Painter::new(ctx),
-            raw_input,
+            egui_ctx: egui::CtxRef::default(),
+            painter: egui_mq::Painter::new(ctx),
+            egui_input: Default::default(),
+            show_egui_demo_windows: true,
+            egui_demo_windows: Default::default(),
         }
+    }
+
+    fn ui(&mut self) {
+        let Self {
+            egui_ctx,
+            show_egui_demo_windows,
+            egui_demo_windows,
+            ..
+        } = self;
+
+        if *show_egui_demo_windows {
+            egui_demo_windows.ui(&egui_ctx);
+        }
+
+        egui::Window::new("Debug").show(&egui_ctx, |ui| {
+            ui.add(egui::Label::new("Egui on Miniquad").text_style(egui::TextStyle::Heading));
+            ui.separator();
+            ui.checkbox(show_egui_demo_windows, "Show egui demo windows");
+            ui.label("Woooohoooo!");
+            if ui.button("Quit").clicked {
+                std::process::exit(0);
+            }
+        });
     }
 }
 
-impl EventHandler for Stage {
-    fn update(&mut self, _ctx: &mut Context) {}
+impl mq::EventHandler for Stage {
+    fn update(&mut self, _ctx: &mut mq::Context) {}
 
-    fn resize_event(&mut self, _ctx: &mut Context, width: f32, height: f32) {
-        self.raw_input.screen_rect = Some(egui::Rect::from_min_size(
-            Default::default(),
-            egui::vec2(width, height),
-        ));
-    }
-
-    fn mouse_button_down_event(
-        &mut self,
-        _ctx: &mut Context,
-        _: miniquad::MouseButton,
-        _x: f32,
-        _y: f32,
-    ) {
-        self.raw_input.mouse_down = true;
-    }
-    fn mouse_button_up_event(
-        &mut self,
-        _ctx: &mut Context,
-        _: miniquad::MouseButton,
-        _x: f32,
-        _y: f32,
-    ) {
-        self.raw_input.mouse_down = false;
-    }
-
-    fn mouse_motion_event(&mut self, ctx: &mut Context, x: f32, y: f32) {
-        self.raw_input.mouse_pos =
-            Some(pos2(x as f32 / ctx.dpi_scale(), y as f32 / ctx.dpi_scale()));
-    }
-
-    fn draw(&mut self, ctx: &mut Context) {
+    fn draw(&mut self, ctx: &mut mq::Context) {
         ctx.clear(Some((1., 1., 1., 1.)), None, None);
         ctx.begin_default_pass(mq::PassAction::clear_color(0.0, 0.0, 0.0, 1.0));
         ctx.end_render_pass();
 
-        // TODO: give all of the raw_input information egui wants so everything works properly
-        self.raw_input.time = Some(miniquad::date::now());
+        egui_mq::on_frame_start(&mut self.egui_input, ctx);
+        self.egui_ctx.begin_frame(self.egui_input.take());
 
-        self.egui_ctx.begin_frame(self.raw_input.take());
-        egui::Window::new("Debug")
-            .default_size(vec2(200.0, 100.0))
-            .show(&self.egui_ctx, |ui| {
-                ui.add(egui::Label::new("Egui on Miniquad").text_style(egui::TextStyle::Heading));
-                ui.separator();
-                ui.label("Woooohoooo!");
-                if ui.button("Quit").clicked {
-                    std::process::exit(0);
-                }
-            });
+        self.ui();
+
         // TODO: handle this output so that hyperlinks, etc. work
         let (_, shapes) = self.egui_ctx.end_frame();
         let paint_jobs = self.egui_ctx.tessellate(shapes);
@@ -92,14 +65,69 @@ impl EventHandler for Stage {
         self.painter
             .paint(ctx, paint_jobs, &self.egui_ctx.texture());
     }
+
+    fn mouse_motion_event(&mut self, ctx: &mut mq::Context, x: f32, y: f32) {
+        self.egui_input.mouse_pos = Some(egui::pos2(
+            x as f32 / ctx.dpi_scale(),
+            y as f32 / ctx.dpi_scale(),
+        ));
+    }
+
+    fn mouse_wheel_event(&mut self, ctx: &mut mq::Context, dx: f32, dy: f32) {
+        self.egui_input.scroll_delta += egui::vec2(dx, dy) * ctx.dpi_scale(); // not quite right speed on Mac for some reason
+    }
+
+    fn mouse_button_down_event(
+        &mut self,
+        _ctx: &mut mq::Context,
+        _: mq::MouseButton,
+        _x: f32,
+        _y: f32,
+    ) {
+        self.egui_input.mouse_down = true;
+    }
+
+    fn mouse_button_up_event(
+        &mut self,
+        _ctx: &mut mq::Context,
+        _: mq::MouseButton,
+        _x: f32,
+        _y: f32,
+    ) {
+        self.egui_input.mouse_down = false;
+    }
+
+    fn char_event(
+        &mut self,
+        _ctx: &mut mq::Context,
+        character: char,
+        _keymods: mq::KeyMods,
+        _repeat: bool,
+    ) {
+        egui_mq::char_event(&mut self.egui_input, character);
+    }
+
+    fn key_down_event(
+        &mut self,
+        _ctx: &mut mq::Context,
+        keycode: mq::KeyCode,
+        keymods: mq::KeyMods,
+        _repeat: bool,
+    ) {
+        egui_mq::key_down_event(&mut self.egui_input, keycode, keymods);
+    }
+
+    fn key_up_event(&mut self, _ctx: &mut mq::Context, keycode: mq::KeyCode, keymods: mq::KeyMods) {
+        egui_mq::key_up_event(&mut self.egui_input, keycode, keymods);
+    }
 }
 
 fn main() {
-    let conf = conf::Conf {
+    let conf = mq::conf::Conf {
         high_dpi: true,
         ..Default::default()
     };
-    miniquad::start(conf, |mut ctx| {
+    mq::start(conf, |mut ctx| {
         mq::UserData::owning(Stage::new(&mut ctx), ctx)
     });
 }
---
+++ src/input.rs
@@ -0,0 +1,137 @@
+use miniquad as mq;
+
+pub fn on_frame_start(egui_input: &mut egui::RawInput, mq_ctx: &mq::Context) {
+    let screen_size_in_pixels = mq_ctx.screen_size();
+    let pixels_per_point = mq_ctx.dpi_scale();
+    let screen_size_in_points =
+        egui::vec2(screen_size_in_pixels.0, screen_size_in_pixels.1) / pixels_per_point;
+    egui_input.screen_rect = Some(egui::Rect::from_min_size(
+        Default::default(),
+        screen_size_in_points,
+    ));
+    egui_input.pixels_per_point = Some(pixels_per_point);
+    egui_input.time = Some(mq::date::now());
+}
+
+/// Pass on miniquad key event to egui
+pub fn char_event(egui_input: &mut egui::RawInput, chr: char) {
+    if is_printable_char(chr) {
+        egui_input.events.push(egui::Event::Text(chr.to_string()));
+    }
+}
+
+/// Pass on miniquad key event to egui
+pub fn key_down_event(egui_input: &mut egui::RawInput, keycode: mq::KeyCode, keymods: mq::KeyMods) {
+    key_event(egui_input, keycode, keymods, true)
+}
+
+/// Pass on miniquad key event to egui
+pub fn key_up_event(egui_input: &mut egui::RawInput, keycode: mq::KeyCode, keymods: mq::KeyMods) {
+    key_event(egui_input, keycode, keymods, false)
+}
+
+/// Pass on miniquad key event to egui
+fn key_event(
+    egui_input: &mut egui::RawInput,
+    keycode: mq::KeyCode,
+    keymods: mq::KeyMods,
+    pressed: bool,
+) {
+    let modifiers = egui_modifiers_from_mq_modifiers(keymods);
+    egui_input.modifiers = modifiers;
+    if let Some(key) = egui_key_from_mq_key(keycode) {
+        egui_input.events.push(egui::Event::Key {
+            key,
+            pressed,
+            modifiers,
+        })
+    }
+}
+
+/// miniquad sends special keys (backspace, delete, F1, ...) as characters.
+/// Ignore those.
+/// We also ignore '\r', '\n', '\t'.
+/// Newlines are handled by the `Key::Enter` event.
+fn is_printable_char(chr: char) -> bool {
+    let is_in_private_use_area = '\u{e000}' <= chr && chr <= '\u{f8ff}'
+        || '\u{f0000}' <= chr && chr <= '\u{ffffd}'
+        || '\u{100000}' <= chr && chr <= '\u{10fffd}';
+
+    !is_in_private_use_area && !chr.is_ascii_control()
+}
+
+fn egui_modifiers_from_mq_modifiers(keymods: mq::KeyMods) -> egui::Modifiers {
+    egui::Modifiers {
+        alt: keymods.alt,
+        ctrl: keymods.ctrl,
+        shift: keymods.shift,
+        mac_cmd: keymods.logo && cfg!(target_os = "macos"),
+        command: if cfg!(target_os = "macos") {
+            keymods.logo
+        } else {
+            keymods.ctrl
+        },
+    }
+}
+
+fn egui_key_from_mq_key(key: mq::KeyCode) -> Option<egui::Key> {
+    Some(match key {
+        mq::KeyCode::Down => egui::Key::ArrowDown,
+        mq::KeyCode::Left => egui::Key::ArrowLeft,
+        mq::KeyCode::Right => egui::Key::ArrowRight,
+        mq::KeyCode::Up => egui::Key::ArrowUp,
+
+        mq::KeyCode::Escape => egui::Key::Escape,
+        mq::KeyCode::Tab => egui::Key::Tab,
+        mq::KeyCode::Backspace => egui::Key::Backspace,
+        mq::KeyCode::Enter => egui::Key::Enter,
+        mq::KeyCode::Space => egui::Key::Space,
+
+        mq::KeyCode::Insert => egui::Key::Insert,
+        mq::KeyCode::Delete => egui::Key::Delete,
+        mq::KeyCode::Home => egui::Key::Home,
+        mq::KeyCode::End => egui::Key::End,
+        mq::KeyCode::PageUp => egui::Key::PageUp,
+        mq::KeyCode::PageDown => egui::Key::PageDown,
+
+        mq::KeyCode::Key0 => egui::Key::Num0,
+        mq::KeyCode::Key1 => egui::Key::Num1,
+        mq::KeyCode::Key2 => egui::Key::Num2,
+        mq::KeyCode::Key3 => egui::Key::Num3,
+        mq::KeyCode::Key4 => egui::Key::Num4,
+        mq::KeyCode::Key5 => egui::Key::Num5,
+        mq::KeyCode::Key6 => egui::Key::Num6,
+        mq::KeyCode::Key7 => egui::Key::Num7,
+        mq::KeyCode::Key8 => egui::Key::Num8,
+        mq::KeyCode::Key9 => egui::Key::Num9,
+
+        mq::KeyCode::A => egui::Key::A,
+        mq::KeyCode::B => egui::Key::B,
+        mq::KeyCode::C => egui::Key::C,
+        mq::KeyCode::D => egui::Key::D,
+        mq::KeyCode::E => egui::Key::E,
+        mq::KeyCode::F => egui::Key::F,
+        mq::KeyCode::G => egui::Key::G,
+        mq::KeyCode::H => egui::Key::H,
+        mq::KeyCode::I => egui::Key::I,
+        mq::KeyCode::J => egui::Key::J,
+        mq::KeyCode::K => egui::Key::K,
+        mq::KeyCode::L => egui::Key::L,
+        mq::KeyCode::M => egui::Key::M,
+        mq::KeyCode::N => egui::Key::N,
+        mq::KeyCode::O => egui::Key::O,
+        mq::KeyCode::P => egui::Key::P,
+        mq::KeyCode::Q => egui::Key::Q,
+        mq::KeyCode::R => egui::Key::R,
+        mq::KeyCode::S => egui::Key::S,
+        mq::KeyCode::T => egui::Key::T,
+        mq::KeyCode::U => egui::Key::U,
+        mq::KeyCode::V => egui::Key::V,
+        mq::KeyCode::W => egui::Key::W,
+        mq::KeyCode::X => egui::Key::X,
+        mq::KeyCode::Y => egui::Key::Y,
+        mq::KeyCode::Z => egui::Key::Z,
+
+        _ => return None,
+    })
+}
