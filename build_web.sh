c2bb39e0ab1cecb31241ad7bd7789ab49aed9f71
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2021-03-24 17:17:44 +0100

    Add wasm-strip step to build_web.sh

--- build_web.sh
+++ build_web.sh
@@ -15,3 +15,6 @@ cp target/wasm32-unknown-unknown/release/examples/${EXAMPLE_NAME}.wasm docs/
 # # Debug:
 # cargo build --example ${EXAMPLE_NAME} --target wasm32-unknown-unknown
 # cp target/wasm32-unknown-unknown/debug/examples/${EXAMPLE_NAME}.wasm docs/
+
+# brew install wabt # to get wasm-strip
+wasm-strip docs/${EXAMPLE_NAME}.wasm
