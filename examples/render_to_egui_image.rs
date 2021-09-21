70065ab65fc5dcb2cafd8dd4fdd63cbbb3e73026
Ben Postlethwaite <post.ben.here@gmail.com>
2021-09-21 09:33:49 -0700

    Example rendering miniquad scene into egui image

---
+++ examples/render_to_egui_image.rs
@@ -0,0 +1,337 @@
+use glam::{vec3, EulerRot, Mat4};
+use {egui_miniquad as egui_mq, miniquad as mq};
+
+struct Stage {
+    egui_mq: egui_mq::EguiMq,
+    offscreen_pipeline: mq::Pipeline,
+    offscreen_bind: mq::Bindings,
+    offscreen_pass: mq::RenderPass,
+    rx: f32,
+    ry: f32,
+}
+
+impl Stage {
+    pub fn new(ctx: &mut mq::Context) -> Stage {
+        let color_img = mq::Texture::new_render_texture(
+            ctx,
+            mq::TextureParams {
+                width: 256,
+                height: 256,
+                format: mq::TextureFormat::RGBA8,
+                ..Default::default()
+            },
+        );
+        let depth_img = mq::Texture::new_render_texture(
+            ctx,
+            mq::TextureParams {
+                width: 256,
+                height: 256,
+                format: mq::TextureFormat::Depth,
+                ..Default::default()
+            },
+        );
+
+        let offscreen_pass = mq::RenderPass::new(ctx, color_img, depth_img);
+
+        #[rustfmt::skip]
+        let vertices: &[f32] = &[
+            /* pos               color                   uvs */
+            -1.0, -1.0, -1.0,    1.0, 0.5, 0.5, 1.0,     0.0, 0.0,
+             1.0, -1.0, -1.0,    1.0, 0.5, 0.5, 1.0,     1.0, 0.0,
+             1.0,  1.0, -1.0,    1.0, 0.5, 0.5, 1.0,     1.0, 1.0,
+            -1.0,  1.0, -1.0,    1.0, 0.5, 0.5, 1.0,     0.0, 1.0,
+
+            -1.0, -1.0,  1.0,    0.5, 1.0, 0.5, 1.0,     0.0, 0.0,
+             1.0, -1.0,  1.0,    0.5, 1.0, 0.5, 1.0,     1.0, 0.0,
+             1.0,  1.0,  1.0,    0.5, 1.0, 0.5, 1.0,     1.0, 1.0,
+            -1.0,  1.0,  1.0,    0.5, 1.0, 0.5, 1.0,     0.0, 1.0,
+
+            -1.0, -1.0, -1.0,    0.5, 0.5, 1.0, 1.0,     0.0, 0.0,
+            -1.0,  1.0, -1.0,    0.5, 0.5, 1.0, 1.0,     1.0, 0.0,
+            -1.0,  1.0,  1.0,    0.5, 0.5, 1.0, 1.0,     1.0, 1.0,
+            -1.0, -1.0,  1.0,    0.5, 0.5, 1.0, 1.0,     0.0, 1.0,
+
+             1.0, -1.0, -1.0,    1.0, 0.5, 0.0, 1.0,     0.0, 0.0,
+             1.0,  1.0, -1.0,    1.0, 0.5, 0.0, 1.0,     1.0, 0.0,
+             1.0,  1.0,  1.0,    1.0, 0.5, 0.0, 1.0,     1.0, 1.0,
+             1.0, -1.0,  1.0,    1.0, 0.5, 0.0, 1.0,     0.0, 1.0,
+
+            -1.0, -1.0, -1.0,    0.0, 0.5, 1.0, 1.0,     0.0, 0.0,
+            -1.0, -1.0,  1.0,    0.0, 0.5, 1.0, 1.0,     1.0, 0.0,
+             1.0, -1.0,  1.0,    0.0, 0.5, 1.0, 1.0,     1.0, 1.0,
+             1.0, -1.0, -1.0,    0.0, 0.5, 1.0, 1.0,     0.0, 1.0,
+
+            -1.0,  1.0, -1.0,    1.0, 0.0, 0.5, 1.0,     0.0, 0.0,
+            -1.0,  1.0,  1.0,    1.0, 0.0, 0.5, 1.0,     1.0, 0.0,
+             1.0,  1.0,  1.0,    1.0, 0.0, 0.5, 1.0,     1.0, 1.0,
+             1.0,  1.0, -1.0,    1.0, 0.0, 0.5, 1.0,     0.0, 1.0
+        ];
+
+        let vertex_buffer = mq::Buffer::immutable(ctx, mq::BufferType::VertexBuffer, &vertices);
+
+        #[rustfmt::skip]
+        let indices: &[u16] = &[
+            0, 1, 2,  0, 2, 3,
+            6, 5, 4,  7, 6, 4,
+            8, 9, 10,  8, 10, 11,
+            14, 13, 12,  15, 14, 12,
+            16, 17, 18,  16, 18, 19,
+            22, 21, 20,  23, 22, 20
+        ];
+
+        let index_buffer = mq::Buffer::immutable(ctx, mq::BufferType::IndexBuffer, &indices);
+
+        let offscreen_bind = mq::Bindings {
+            vertex_buffers: vec![vertex_buffer.clone()],
+            index_buffer: index_buffer.clone(),
+            images: vec![],
+        };
+
+        let offscreen_shader = mq::Shader::new(
+            ctx,
+            offscreen_shader::VERTEX,
+            offscreen_shader::FRAGMENT,
+            offscreen_shader::meta(),
+        )
+        .unwrap();
+
+        let offscreen_pipeline = mq::Pipeline::with_params(
+            ctx,
+            &[mq::BufferLayout {
+                stride: 36,
+                ..Default::default()
+            }],
+            &[
+                mq::VertexAttribute::new("pos", mq::VertexFormat::Float3),
+                mq::VertexAttribute::new("color0", mq::VertexFormat::Float4),
+            ],
+            offscreen_shader,
+            mq::PipelineParams {
+                depth_test: mq::Comparison::LessOrEqual,
+                depth_write: true,
+                ..Default::default()
+            },
+        );
+
+        Stage {
+            egui_mq: egui_mq::EguiMq::new(ctx),
+            offscreen_pipeline,
+            offscreen_bind,
+            offscreen_pass,
+            rx: 0.,
+            ry: 0.,
+        }
+    }
+
+    fn ui(&mut self, texture_id: egui::TextureId) {
+        let Self { egui_mq, .. } = self;
+
+        let egui_ctx = egui_mq.egui_ctx();
+        egui::Window::new("egui ❤ miniquad").show(egui_ctx, |ui| {
+            ui.image(texture_id, egui::Vec2::new(140.0, 140.0));
+            #[cfg(not(target_arch = "wasm32"))]
+            {
+                if ui.button("Quit").clicked() {
+                    std::process::exit(0);
+                }
+            }
+        });
+    }
+}
+
+impl mq::EventHandler for Stage {
+    fn update(&mut self, _ctx: &mut mq::Context) {}
+
+    fn draw(&mut self, ctx: &mut mq::Context) {
+        let (width, height) = ctx.screen_size();
+        let proj = Mat4::perspective_rh_gl(60.0f32.to_radians(), width / height, 0.01, 10.0);
+        let view = Mat4::look_at_rh(
+            vec3(0.0, 1.5, 3.0),
+            vec3(0.0, 0.0, 0.0),
+            vec3(0.0, 1.0, 0.0),
+        );
+        let view_proj = proj * view;
+
+        self.rx += 0.01;
+        self.ry += 0.03;
+        let model = Mat4::from_euler(EulerRot::YXZ, self.rx, self.ry, 0.);
+
+        let vs_params = display_shader::Uniforms {
+            mvp: view_proj * model,
+        };
+
+        // the offscreen pass, rendering an rotating, untextured cube into a render target image
+        ctx.begin_pass(
+            self.offscreen_pass,
+            mq::PassAction::clear_color(1.0, 1.0, 1.0, 1.),
+        );
+        ctx.apply_pipeline(&self.offscreen_pipeline);
+        ctx.apply_bindings(&self.offscreen_bind);
+        ctx.apply_uniforms(&vs_params);
+        ctx.draw(0, 36, 1);
+        ctx.end_render_pass();
+
+        // Extract texture from offscreen render pass
+        let mq_texture = self.offscreen_pass.texture(ctx);
+
+        // create egui TextureId from Miniquad GL texture Id
+        let egui_texture_id = egui::TextureId::User(mq_texture.gl_internal_id() as u64);
+
+        ctx.clear(Some((1., 1., 1., 1.)), None, None);
+        ctx.begin_default_pass(mq::PassAction::clear_color(0.0, 0.0, 0.0, 1.0));
+        ctx.end_render_pass();
+
+        self.egui_mq.begin_frame(ctx);
+
+        // pass in TextureId to be rendered in egui ui.image
+        self.ui(egui_texture_id);
+        self.egui_mq.end_frame(ctx);
+
+        // Draw things behind egui here
+
+        self.egui_mq.draw(ctx);
+
+        // Draw things in front of egui here
+
+        ctx.commit_frame();
+    }
+
+    fn mouse_motion_event(&mut self, ctx: &mut mq::Context, x: f32, y: f32) {
+        self.egui_mq.mouse_motion_event(ctx, x, y);
+    }
+
+    fn mouse_wheel_event(&mut self, ctx: &mut mq::Context, dx: f32, dy: f32) {
+        self.egui_mq.mouse_wheel_event(ctx, dx, dy);
+    }
+
+    fn mouse_button_down_event(
+        &mut self,
+        ctx: &mut mq::Context,
+        mb: mq::MouseButton,
+        x: f32,
+        y: f32,
+    ) {
+        self.egui_mq.mouse_button_down_event(ctx, mb, x, y);
+    }
+
+    fn mouse_button_up_event(
+        &mut self,
+        ctx: &mut mq::Context,
+        mb: mq::MouseButton,
+        x: f32,
+        y: f32,
+    ) {
+        self.egui_mq.mouse_button_up_event(ctx, mb, x, y);
+    }
+
+    fn char_event(
+        &mut self,
+        _ctx: &mut mq::Context,
+        character: char,
+        _keymods: mq::KeyMods,
+        _repeat: bool,
+    ) {
+        self.egui_mq.char_event(character);
+    }
+
+    fn key_down_event(
+        &mut self,
+        ctx: &mut mq::Context,
+        keycode: mq::KeyCode,
+        keymods: mq::KeyMods,
+        _repeat: bool,
+    ) {
+        self.egui_mq.key_down_event(ctx, keycode, keymods);
+    }
+
+    fn key_up_event(&mut self, _ctx: &mut mq::Context, keycode: mq::KeyCode, keymods: mq::KeyMods) {
+        self.egui_mq.key_up_event(keycode, keymods);
+    }
+}
+
+fn main() {
+    miniquad::start(mq::conf::Conf::default(), |mut ctx| {
+        mq::UserData::owning(Stage::new(&mut ctx), ctx)
+    });
+}
+
+mod display_shader {
+    use miniquad as mq;
+
+    pub const VERTEX: &str = r#"#version 100
+    attribute vec4 pos;
+    attribute vec4 color0;
+    attribute vec2 uv0;
+
+    varying lowp vec4 color;
+    varying lowp vec2 uv;
+
+    uniform mat4 mvp;
+
+    void main() {
+        gl_Position = mvp * pos;
+        color = color0;
+        uv = uv0;
+    }
+    "#;
+
+    pub const FRAGMENT: &str = r#"#version 100
+    varying lowp vec4 color;
+    varying lowp vec2 uv;
+
+    uniform sampler2D tex;
+
+    void main() {
+        gl_FragColor = color * texture2D(tex, uv);
+    }
+    "#;
+
+    pub fn meta() -> mq::ShaderMeta {
+        mq::ShaderMeta {
+            images: vec!["tex".to_string()],
+            uniforms: mq::UniformBlockLayout {
+                uniforms: vec![mq::UniformDesc::new("mvp", mq::UniformType::Mat4)],
+            },
+        }
+    }
+
+    #[repr(C)]
+    pub struct Uniforms {
+        pub mvp: glam::Mat4,
+    }
+}
+
+mod offscreen_shader {
+    use miniquad as mq;
+
+    pub const VERTEX: &str = r#"#version 100
+    attribute vec4 pos;
+    attribute vec4 color0;
+
+    varying lowp vec4 color;
+
+    uniform mat4 mvp;
+
+    void main() {
+        gl_Position = mvp * pos;
+        color = color0;
+    }
+    "#;
+
+    pub const FRAGMENT: &str = r#"#version 100
+    varying lowp vec4 color;
+
+    void main() {
+        gl_FragColor = color;
+    }
+    "#;
+
+    pub fn meta() -> mq::ShaderMeta {
+        mq::ShaderMeta {
+            images: vec![],
+            uniforms: mq::UniformBlockLayout {
+                uniforms: vec![mq::UniformDesc::new("mvp", mq::UniformType::Mat4)],
+            },
+        }
+    }
+}
