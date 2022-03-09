737b34b93bf7d2a6c0d3293d268dd6da84e272d7
Aspect <ttmmiizz100@gmail.com>
2022-03-08 21:43:48 -0500

    Fix color issues

--- src/painter.rs
+++ src/painter.rs
@@ -29,6 +29,7 @@ impl Painter {
                     BlendFactor::One,
                     BlendFactor::OneMinusValue(BlendValue::SourceAlpha),
                 )),
+                cull_face: miniquad::CullFace::Nothing,
                 ..Default::default()
             },
         );
@@ -124,7 +125,7 @@ impl Painter {
                         "Mismatch between texture size and texel count"
                     );
 
-                    let gamma = 1.0;
+                    let gamma = 1.0 / 2.2;
                     let data: Vec<u8> = image
                         .srgba_pixels(gamma)
                         .flat_map(|a| a.to_array())
@@ -285,6 +286,19 @@ mod shader {
         return vec4(linear_from_srgb(srgba.rgb), srgba.a / 255.0);
     }
 
+    // 0-255 sRGB  from  0-1 linear
+    vec3 srgb_from_linear(vec3 rgb) {
+        bvec3 cutoff = lessThan(rgb, vec3(0.0031308));
+        vec3 lower = rgb * vec3(3294.6);
+        vec3 higher = vec3(269.025) * pow(rgb, vec3(1.0 / 2.4)) - vec3(14.025);
+        return mix(higher, lower, vec3(cutoff));
+    }
+
+    // 0-255 sRGBA  from  0-1 linear
+    vec4 srgba_from_linear(vec4 rgba) {
+        return vec4(srgb_from_linear(rgba.rgb), 255.0 * rgba.a);
+    }
+
     void main() {
         gl_Position = vec4(
             2.0 * a_pos.x / u_screen_size.x - 1.0,
@@ -293,7 +307,8 @@ mod shader {
             1.0);
 
         v_tc = a_tc;
-        v_rgba = linear_from_srgba(a_srgba);
+        v_rgba = a_srgba / 255.0;
+        v_rgba.a = v_rgba.a * v_rgba.a;
     }
     "#;
 
@@ -334,28 +349,29 @@ mod shader {
     void main() {
         // We must decode the colors, since WebGL1 doesn't come with sRGBA textures:
         vec4 texture_rgba = linear_from_srgba(texture2D(u_sampler, v_tc) * 255.0);
-        /// Multiply vertex color with texture color (in linear space).
-        gl_FragColor = v_rgba * texture_rgba;
 
         // WebGL1 doesn't support linear blending in the framebuffer,
         // so we do a hack here where we change the premultiplied alpha
         // to do the multiplication in gamma space instead:
 
         // Unmultiply alpha:
-        if (gl_FragColor.a > 0.0) {
-            gl_FragColor.rgb /= gl_FragColor.a;
+        if (texture_rgba.a > 0.0) {
+            texture_rgba.rgb /= texture_rgba.a;
         }
 
         // Empiric tweak to make e.g. shadows look more like they should:
-        gl_FragColor.a *= sqrt(gl_FragColor.a);
+        texture_rgba.a *= sqrt(texture_rgba.a);
 
         // To gamma:
-        gl_FragColor = srgba_from_linear(gl_FragColor) / 255.0;
+        texture_rgba = srgba_from_linear(texture_rgba) / 255.0;
 
         // Premultiply alpha, this time in gamma space:
-        if (gl_FragColor.a > 0.0) {
-            gl_FragColor.rgb *= gl_FragColor.a;
+        if (texture_rgba.a > 0.0) {
+            texture_rgba.rgb *= texture_rgba.a;
         }
+
+        /// Multiply vertex color with texture color (in linear space).
+        gl_FragColor = v_rgba * texture_rgba;
     }
     "#;
 
