00c104120cd23f8a8d8014e7cbdfc7d6a2efe329
Piotr Bartoszewicz <piotrek15.pb@gmail.com>
2024-12-31 00:17:14 +0100

    use macro in examples

--- examples/render_to_egui_image.rs
+++ examples/render_to_egui_image.rs
@@ -202,33 +202,8 @@ impl mq::EventHandler for Stage {
         self.mq_ctx.commit_frame();
     }
 
-    fn mouse_motion_event(&mut self, x: f32, y: f32) {
-        self.egui_mq.mouse_motion_event(x, y);
-    }
-
-    fn mouse_wheel_event(&mut self, dx: f32, dy: f32) {
-        self.egui_mq.mouse_wheel_event(dx, dy);
-    }
-
-    fn mouse_button_down_event(&mut self, mb: mq::MouseButton, x: f32, y: f32) {
-        self.egui_mq.mouse_button_down_event(mb, x, y);
-    }
-
-    fn mouse_button_up_event(&mut self, mb: mq::MouseButton, x: f32, y: f32) {
-        self.egui_mq.mouse_button_up_event(mb, x, y);
-    }
-
-    fn char_event(&mut self, character: char, _keymods: mq::KeyMods, _repeat: bool) {
-        self.egui_mq.char_event(character);
-    }
-
-    fn key_down_event(&mut self, keycode: mq::KeyCode, keymods: mq::KeyMods, _repeat: bool) {
-        self.egui_mq.key_down_event(keycode, keymods);
-    }
-
-    fn key_up_event(&mut self, keycode: mq::KeyCode, keymods: mq::KeyMods) {
-        self.egui_mq.key_up_event(keycode, keymods);
-    }
+    // generate boilerplate code
+    egui_mq::default_egui_mq!(egui_mq);
 }
 
 fn main() {
